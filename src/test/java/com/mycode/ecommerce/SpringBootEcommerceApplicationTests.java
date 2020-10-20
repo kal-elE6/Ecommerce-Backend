4e2386af5bb2c50e0a2e788bef3dae9e68e85bdd
Jason Pis-an <jpisan@Jasons-MacBook-Air.local>
2020-10-21 02:00:10 +0800

    initial commit

---
+++ src/test/java/com/mycode/ecommerce/SpringBootEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycode.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
