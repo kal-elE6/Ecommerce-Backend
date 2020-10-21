228548fec9783e5bbee5e0f736a4fdf40a025ecb
Jason Pis-an <jpisan@Jasons-MacBook-Air.local>
2020-10-22 02:18:16 +0800

    CrossOrigin annotation to integrate angular and springboot

--- src/main/java/com/mycode/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/mycode/ecommerce/dao/ProductCategoryRepository.java
@@ -3,7 +3,10 @@ package com.mycode.ecommerce.dao;
 import com.mycode.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+//add this annotation to intigrate angular and springboot
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- src/main/java/com/mycode/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/mycode/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,9 @@ package com.mycode.ecommerce.dao;
 
 import com.mycode.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+//add this annotation to intigrate angular and springboot
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
