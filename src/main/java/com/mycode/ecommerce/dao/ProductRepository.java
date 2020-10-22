8352c4dda289c61b3e7bab6ba6a85c33055ea6e4
Jason Pis-an <jpisan@Jasons-MacBook-Air.local>
2020-10-23 03:11:51 +0800

    Search for products by category code - Update SpringBoot

--- src/main/java/com/mycode/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/mycode/ecommerce/dao/ProductRepository.java
@@ -1,10 +1,17 @@
 package com.mycode.ecommerce.dao;
 
 import com.mycode.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 //add this annotation to intigrate angular and springboot
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
